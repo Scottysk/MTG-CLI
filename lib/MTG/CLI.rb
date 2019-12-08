e7e712591e19ad9a53d33602f33fd1325d16c3eb
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-08 23:45:42 +0000

    first commit

---
+++ lib/MTG/CLI.rb
@@ -0,0 +1,8 @@
+require "MTG/CLI/version"
+
+module MTG
+  module CLI
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/MTG/CLI/version.rb
@@ -0,0 +1,5 @@
+module MTG
+  module CLI
+    VERSION = "0.1.0"
+  end
+end
