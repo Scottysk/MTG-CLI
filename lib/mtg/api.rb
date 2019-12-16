bb29bcfad24ec792e53cc1c0e1526c2a431bc550
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-16 23:48:26 +0000

    temp

--- lib/mtg/api.rb
+++ lib/mtg/api.rb
@@ -5,11 +5,14 @@ class MTG::API
   
   
   def self.query_cards_db(query)
-    results = RestClient.get("https://api.magicthegathering.io/v1/#{query}")
+    results = RestClient.get("https://api.magicthegathering.io/v1/cards")
     
     json = JSON.parse(results)
     binding.pry
-    
+    json["cards"].each do |card_hash|
+      MTG::Cards.new(card_hash)
+    end
+
   end
 
   
--- lib/mtg/cards.rb
+++ lib/mtg/cards.rb
@@ -3,18 +3,14 @@ class MTG::Cards
   attr_accessor :name, :manaCost, :rarity, :type, :setName, :text, :artist
   @@all = []
   
-  def initialize(name, manaCost, rarity, type, setName, text, artist)
-    @name = name 
-    @manaCost = manaCost
-    @rarity = rarity
-    @type = type
-    @setName = setName
-    @text = text
-    @artist = artist
-    @@all << self
+  def initialize(args)
+    args.each do |k, v|
+      self.send("#{k}=", v) if self.respond_to?(k)
+      end
+      @@all << self
   end
   
   def self.all
     @@all
   end
-end
+ end
