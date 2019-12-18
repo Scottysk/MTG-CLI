a54ebfacdccd786ec24ef2b8b6f122aef84440fd
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-12-18 22:08:49 +0000

    temp

--- lib/mtg/cli.rb
+++ lib/mtg/cli.rb
@@ -5,13 +5,31 @@ class MTG::Cli
     puts "Hello, welcome to the Magic the Gathering card program!"
     puts "----------------------------------"
     menu
+    list_cards
   end
   
   def menu
-    puts "What card would you like to look for?"
-    
+    input = nil
+    while input != "exit"
+    puts "Press '1' to see a list 100 Magic the Gathering cards or type exit to close the program."
     input = gets.strip
-    MTG::API.query_cards_db(input)
+    if input.to_i > 0
+      puts @cards[input.to_i-1]
+    else
+      puts "Closing the program."
+    end
   end
+    
+    
+#    puts "What card would you like to look for?"
+#    input = gets.strip
+#    MTG::API.query_cards_db(input)
+  end
+  
+  def list_cards
+    @cards = MTG::Cards.all.each.with_index(1) do |m, i|
+      puts "#{i}. #{m.name}."
+  end
+end
      
 end
